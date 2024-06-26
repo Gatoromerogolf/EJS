1295c26978f49467de05ec95a79130c877e67db3
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-06-26 15:00:18 -0300

    tabla02

--- public/datos/tabla02.js
+++ public/datos/tabla02.js
@@ -6,14 +6,14 @@ const tabla02 = [
   [0, 20, 40, 60],
   [0, 20, 20],
   [10, 20, 20, 10, 10, 10, 0],
-  [10, 20],
-  [10, 0],
-  [20, 0],
-  [20, 0],
-  [10, 0],
-  [20, 0],
-  [20, 0],
-  [20, 0],
-  [40, 0],
-  [20, 0]
+  [10, 20], // 8. frecuencia de reuniones
+  [10, 0], // 9. reuniones adicionales ad hoc
+  [20, 0], // 10. hay Advisory Board
+  [0, 10, 15, 20], // 11. AB participa en reuniones
+  [0, 5, 7.5, 10], // 12.func documentadas
+  [0, 10, 15, 20], // 13. proposito vision mision
+  [0, 10, 15, 20], // 14. codigo gob corpor
+  [0, 10, 15, 20], // 15. reglamento direct
+  [0, 20, 30, 40], // 16. remunerac
+  [0, 10, 15, 20] // 17. actas
 ]
